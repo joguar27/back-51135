92e489990208b6216009f5dc30c2619e029191c1
jorge <jorgegusarruda@gmail.com>
2023-02-20 12:56:33 -0300

    commit 2

---
+++ clase_01/index.js
@@ -0,0 +1,27 @@
+// let nombre = 'Ana';
+
+// nombre = 'Camila';
+
+
+// const objeto = {    
+//     nombre: "Jorge",
+//     apellido: "Arruda"
+// };
+
+// const array = [1, 2, 3, 4, 'hola'];
+
+// array = [true, 'hola'];
+
+// console.log(typeof (array));
+// console.log(array);
+
+//copia por referencia
+let obj1 = {
+    nombre: "Jorge",
+    edad: 57
+}
+
+console.log(obj1);
+
+let obj2 = obj1;
+console.log(obj2);
